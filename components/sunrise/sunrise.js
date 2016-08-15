05923042cdee2986d4ba3bbee90953085a7f1367
Aaron Barker <aaron.barker@vivintsolar.com>
2016-08-15 13:52:33 -0600

    moving sunrise/sunset stuff into components folder to match structure of other projects

--- components/sunrise/sunrise.js
+++ components/sunrise/sunrise.js
@@ -1,7 +1,7 @@
-var menu = document.querySelector('.menu');
+var menu = document.querySelector('.sunrise-menu');
 var trigger = document.querySelector('.sunrise__menu--trigger');
-var overlay = document.querySelector('.menu__overlay');
-var close = document.querySelector('.menu__close');
+var overlay = document.querySelector('.sunrise-menu__overlay');
+var close = document.querySelector('.sunrise-menu__close');
 
 trigger.addEventListener('click', function(e) {
   e.preventDefault();
