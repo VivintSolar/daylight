3c0efb0857908c0e559f2f68a0813899a22c3144
Aaron Barker <aaron.barker@vivintsolar.com>
2016-09-08 15:50:23 -0600

    first draft of some updates for a new take at a header and footer

--- components/sunrise/sunrise.js
+++ components/sunrise/sunrise.js
@@ -1,24 +1,24 @@
-var menu = document.querySelector('.sunrise-menu');
-var trigger = document.querySelector('.sunrise__menu--trigger');
-var overlay = document.querySelector('.sunrise-menu__overlay');
-var close = document.querySelector('.sunrise-menu__close');
-
-trigger.addEventListener('click', function(e) {
-  e.preventDefault();
-  toggleMenu();
-});
-
-overlay.addEventListener('click', function(e) {
-  e.preventDefault();
-  toggleMenu();
-});
-
-close.addEventListener('click', function(e) {
-  e.preventDefault();
-  toggleMenu();
-});
-
-function toggleMenu() {
-  overlay.classList.toggle('open');
-  menu.classList.toggle('open');
-}
+// var menu = document.querySelector('.sunrise-menu');
+// var trigger = document.querySelector('.sunrise__menu--trigger');
+// var overlay = document.querySelector('.sunrise-menu__overlay');
+// var close = document.querySelector('.sunrise-menu__close');
+// 
+// trigger.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   toggleMenu();
+// });
+// 
+// overlay.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   toggleMenu();
+// });
+// 
+// close.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   toggleMenu();
+// });
+// 
+// function toggleMenu() {
+//   overlay.classList.toggle('open');
+//   menu.classList.toggle('open');
+// }
