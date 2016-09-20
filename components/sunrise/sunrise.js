f85722cedf212b49780d95fc3b699ee984d9ba1e
Clark McLean <clarkmclean@gmail.com>
2016-09-20 16:11:28 -0600

    Revert "display phone number with menu on small site"
    This reverts commit 5b2d9495accfa48b65c3e3c9bbcd29e3ac38ea0c.

--- components/sunrise/sunrise.js
+++ components/sunrise/sunrise.js
@@ -5,24 +5,20 @@ var close = document.querySelector('.sunrise-menu__close');
 
 trigger.addEventListener('click', function(e) {
   e.preventDefault();
-  console.log('trigger');
   toggleMenu();
 });
 
 overlay.addEventListener('click', function(e) {
   e.preventDefault();
-  console.log('overlay');
   toggleMenu();
 });
 
 close.addEventListener('click', function(e) {
   e.preventDefault();
-  console.log('close');
   toggleMenu();
 });
 
 function toggleMenu() {
   overlay.classList.toggle('open');
   menu.classList.toggle('open');
-  console.log('toggle');
 }
